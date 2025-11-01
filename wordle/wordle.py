88293d62d0e4476580cb98a7dece3e0a8937d2d6
israelPlaza <israel.plaza.mayoral@gmail.com>
2025-11-01 16:01:05 +0100

    ejercico validacion

---
+++ wordle/wordle.py
@@ -0,0 +1,9 @@
+import secrets
+#creamos la lista de palabras
+lista_palabras = ["LENIN","TOMAS","CARRO","CASAS","TRATO"]
+
+#Paraseleccionar la palabra secreta
+Palabra_secreta = secrets.choice(lista_palabras)
+
+palabra=""
+intentos=0
